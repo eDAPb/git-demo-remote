8fbddf0b7add4d5d54aba7920a442b04f57b709f
eDAPb <64708153+eDAPb@users.noreply.github.com>
2022-05-16 15:07:45 -0400

    added toString() method

--- GoodMorning.java
+++ GoodMorning.java
@@ -1,5 +1,10 @@
 public class GoodMorning {
     public static void print(String name) {
-        System.out.printf("Good morning %s!", name);
+        System.out.printf("Good morning %s!" name);
+    }
+    
+    public static void toString()
+    {
+        System.out.println("Good morning.");
     }
 }
